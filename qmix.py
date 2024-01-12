ecfe053f723f3354f2cfc81f0272ee9c2b9bb236
dodoseung <sok7819@gmail.com>
2024-01-12 21:36:48 +0900

    qmix

--- qmix.py
+++ qmix.py
@@ -13,16 +13,41 @@ class MixingNet(nn.Module):
         self.state_num = state_num
         self.hidden_dim = hidden_dim
 
-        self.hyper_w1 = nn.Linear(self.state_num, self.hidden_dim)
+        # The weights and bias of the hypernetwork (non-negative) for the first layer
+        self.hyper_w1 = nn.Linear(self.state_num, self.agent_num * self.hidden_dim)
+        self.hyper_b1 = nn.Linear(self.state_num, self.hidden_dim)
+        
+        # The weights and bias of the hypernetwork (non-negative) for the first layer
         self.hyper_w2 = nn.Linear(self.state_num, self.hidden_dim)
-        self.hyper_w3 = nn.Linear(self.state_num, self.hidden_dim)
-        self.hyper_w4_1 = nn.Linear(self.state_num, self.hidden_dim)
-        self.hyper_w4_2 = nn.Linear(self.state_num, self.hidden_dim)
-
-        return 0
+        self.hyper_b2 = nn.Sequential(nn.Linear(self.state_num, self.hidden_dim),
+                                    nn.ReLU(),
+                                    nn.Linear(self.hidden_dim, 1))
     
-    def forward(self, state, q_values):
-        return 0
+    def forward(self, states, q_values):
+        batch_size = states.shape(0)
+        
+        # Reshaping
+        states = states.reshape(-1, self.state_num)
+        q_values = q_values.reshape(-1, 1, self.agent_num)
+
+        # First layer
+        w1 = torch.abs(self.hyper_w1(states))
+        w1 = w1.reshape(-1, self.agent_num, self.hidden_dim)
+        b1 = self.hyper_b1(states)
+        b1 = b1.reshape(-1, 1, self.hidden_dim)
+        x = F.elu(torch.bmm(q_values, w1) + b1)
+
+        # Second layer
+        w2 = torch.abs(self.hyper_w2(states))
+        w2 = w2.reshape(-1, self.hidden_dim, 1)
+        b2 = self.hyper_b2(states)
+        b2 = b2.reshape(-1, 1, 1)
+        y = torch.bmm(x, w2) + b2
+
+        # Reshaping
+        q_tot = y.reshape(batch_size, -1, 1)
+
+        return q_tot
     
 class AgentNet(nn.Module):
     def __init__(self, obs_num, action_num, rnn_dim):
