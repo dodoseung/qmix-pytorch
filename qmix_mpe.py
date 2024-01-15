3b67bf4a56a26aeebf230da8cabc4439f012ce68
dodoseung <sok7819@gmail.com>
2024-01-15 16:32:55 +0900

    init

--- qmix_mpe.py
+++ qmix_mpe.py
@@ -1,15 +1,33 @@
-from PettingZoo.pettingzoo.mpe import simple_adversary_v3
+from environments.custom_environment_v0 import CustomActionMaskedEnvironment
 
-env = simple_adversary_v3.env(render_mode='human')
+if __name__ == "__main__":
+    env = CustomActionMaskedEnvironment()
+    max_episode = 1
+    max_step = 10
+    
+    for i in range(max_episode):
+        
+        observation, info = env.reset()
+        for step in range(max_step):
+            actions = {a: env.action_space(a).sample() for a in env.possible_agents}
+            observation, reward, termination, info = env.step(actions)
 
-env.reset()
-for agent in env.agent_iter():
-    observation, reward, termination, truncation, info = env.last()
+            # Termination conditions
+            if all(termination[a] for a in env.possible_agents):
+                break
+            
+            # Rendering
+            env.render()
 
-    if termination or truncation:
-        action = None
-    else:
-        action = env.action_space(agent).sample() # this is where you would insert your policy
-    print(action)
-    env.step(action)
-env.close()
+    env.close
+
+    # for agent in env.agent_iter():
+    #     observation, reward, termination, truncation, info = env.last()
+
+    #     if termination or truncation:
+    #         action = None
+    #     else:
+    #         action = env.action_space(agent).sample() # this is where you would insert your policy
+    #     print(action)
+    #     env.step(action)
+    # env.close()
